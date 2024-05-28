b6aa5587fbd6f3bcb3c9c3c95801be15cb4a1961
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-28 14:57:21 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  assume_role_policy = var.assume_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each = { for policy in var.policy_arns : policy => policy }
+
+  role       = aws_iam_role.this.name
+  policy_arn = each.value
+}
