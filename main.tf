6bf9e315446a9b1d5d22157c44b46dadaf0e7ca1
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-05-28 21:06:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
-resource "aws_iam_role" "this" {
-  name               = var.role_name
-  assume_role_policy = var.assume_role_policy
+data "aws_iam_role" "this" {
+  name = var.role_name
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
   for_each = { for idx, policy_arn in var.policy_arns : idx => policy_arn }
-  role       = aws_iam_role.this.name
+  role       = data.aws_iam_role.this.name
   policy_arn = each.value
 }
 
-// Removed the duplicate output definitions from here
+// No output definitions here
