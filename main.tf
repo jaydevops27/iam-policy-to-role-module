96ab61883428d8bc694e49697f448bce8310e93e
jaydevops27 <106821114+jaydevops27@users.noreply.github.com>
2024-06-03 13:11:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-data "aws_iam_role" "this" {
-  name = var.role_name
-}
-
 resource "aws_iam_role_policy_attachment" "this" {
   for_each = { for idx, policy_arn in var.policy_arns : idx => policy_arn }
   role       = data.aws_iam_role.this.name
   policy_arn = each.value
 }
-
-// No output definitions here
